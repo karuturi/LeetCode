f0b4e5e41109fcd3502be776d72ce6fdd8adcd7e
Rajani Karuturi <rajanikaruturi@gmail.com>
2021-10-11 16:46:48 +0530

    not so intuitive iterative solution for k combinations from n

--- src/Solution77.java
+++ src/Solution77.java
@@ -4,18 +4,47 @@
 
 import java.util.List;
 import java.util.LinkedList;
+import java.util.ArrayList;
 
 public class Solution77 {
     public static void main(String[] args) {
+        // System.out.println(new Solution77().combine(20, 16));
         System.out.println(new Solution77().combine(3, 2));
     }
 
     public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> allcombinations = combineLesstThanK(n, k);
-        
-        allcombinations.removeIf(combination -> combination.size() != k);
-        return allcombinations;
+		List<List<Integer>> combinations = new ArrayList<List<Integer>>();
+
+		int[] combination = new int[k];
+		for (int i = 0; i < k; i++) {
+			combination[i] = i;
+		}
+
+		while (combination[k - 1] < n) {
+			List<Integer> comblist = new ArrayList<Integer>();
+			for(int i=0; i<combination.length; i++) {
+				comblist.add(combination[i]+1);
+			}
+			combinations.add(comblist);
+			int t = k - 1;
+			while (t != 0 && combination[t] == n - k + t) {
+				t--;
+			}
+			combination[t]++;
+			for (int i = t + 1; i < k; i++) {
+				combination[i] = combination[i - 1] + 1;
+			}
+		}
+
+		return combinations;        
     }
+
+    // public List<List<Integer>> combine(int n, int k) {
+    //     List<List<Integer>> allcombinations = combineLesstThanK(n, k);
+        
+    //     allcombinations.removeIf(combination -> combination.size() != k);
+    //     return allcombinations;
+    // }
 
     public List<List<Integer>> combineLesstThanK(int n, int k) {
 		List<List<Integer>> combinations = new LinkedList<>();
@@ -27,12 +56,13 @@ public class Solution77 {
 
 		List<List<Integer>> combinationsWithoutN = combineLesstThanK(n-1, k);
 		for(List<Integer> combinationWithoutN: combinationsWithoutN ) {
-            if(combinationWithoutN.size() < k) {
+            int size = combinationWithoutN.size();
+            if(size < k ) {
                 LinkedList<Integer> combinationWithN = new LinkedList<>(combinationWithoutN);
                 combinationWithN.add(n);
                 combinations.add(combinationWithN);
                 combinations.add(combinationWithoutN);
-            } else if (combinationWithoutN.size() == k) {
+            } else if (size == k) {
                 combinations.add(combinationWithoutN);
             }
 		}
