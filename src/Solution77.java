2849cad242a862adb9fe128acba701855acd775d
Rajani Karuturi <rajanikaruturi@gmail.com>
2021-10-11 15:33:45 +0530

    all combinations of 1 to n less than or equla to k

--- src/Solution77.java
+++ src/Solution77.java
@@ -7,24 +7,29 @@ import java.util.LinkedList;
 
 public class Solution77 {
     public static void main(String[] args) {
-        System.out.println(new Solution77().combine(3, 0));
+        System.out.println(new Solution77().combine(3, 2));
     }
 
     public List<List<Integer>> combine(int n, int k) {
 		List<List<Integer>> combinations = new LinkedList<>();
 
-		if(n == 0 ) {
+		if(n == 0 || k==0 || n < k) {
 			combinations.add(new LinkedList<Integer>());
 			return combinations;
 		}
 
 		List<List<Integer>> combinationsWithoutN = combine(n-1, k);
 		for(List<Integer> combinationWithoutN: combinationsWithoutN ) {
-            LinkedList<Integer> combinationWithN = new LinkedList<>(combinationWithoutN);
-            combinationWithN.add(n);
-			combinations.add(combinationWithN);
+            if(combinationWithoutN.size() < k) {
+                LinkedList<Integer> combinationWithN = new LinkedList<>(combinationWithoutN);
+                combinationWithN.add(n);
+                combinations.add(combinationWithN);
+                combinations.add(combinationWithoutN);
+            } else if (combinationWithoutN.size() == k) {
+                combinations.add(combinationWithoutN);
+            }
 		}
-		combinations.addAll(combinationsWithoutN);
+		// combinations.addAll(combinationsWithoutN);
 		return combinations;
 		
 	}
